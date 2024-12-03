ef26872b79b3946d71dc9529c6548e0632d55fa7
jgabaut <14669437-jgabaut@users.noreply.gitlab.com>
2024-12-03 02:59:53 +0100

    chore: use SHORT_PROJECT_VERSION for window title

--- src/main.rs
+++ src/main.rs
@@ -34,9 +34,11 @@ fn main() {
 
     let mut main_state = MainState::new();
 
+    let window_title = format!("esox v{SHORT_PROJECT_VERSION}");
+
     let (mut rl, thread) = raylib::init()
         .size(ESOX_SCREEN_WIDTH, ESOX_SCREEN_HEIGHT)
-        .title("Controller & View Example")
+        .title(&window_title)
         .resizable()
         .build();
 
