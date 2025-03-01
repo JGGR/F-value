836b45c8ae168fa67ea5ccac2274b65f30b49212
jgabaut <14669437-jgabaut@users.noreply.gitlab.com>
2025-03-01 16:43:23 +0100

    fix: update engines::niseci::x2 to only count if specie.specie_attesa

--- src/engines/niseci/x2/mod.rs
+++ src/engines/niseci/x2/mod.rs
@@ -36,7 +36,7 @@ pub fn calculate_x2(campionamento: &CampionamentoNISECI, anagrafica: &Anagrafica
 
   let mut specie_campionate_set:HashMap<String, bool> = HashMap::new();
   for cattura in &campionamento.campionamento {
-    if cattura.specie.tipo_autoctono == 1 || cattura.specie.tipo_autoctono == 2 {
+    if cattura.specie.specie_attesa && (cattura.specie.tipo_autoctono == 1 || cattura.specie.tipo_autoctono == 2) {
       match specie_campionate_set.entry(cattura.specie.id.clone()) {
           Entry::Occupied(_) => {},
           Entry::Vacant(vacant_entry) => {
@@ -48,8 +48,8 @@ pub fn calculate_x2(campionamento: &CampionamentoNISECI, anagrafica: &Anagrafica
 
   let tot_specie_attese_trovate = specie_campionate_set.len();
 
-  let result = (0.6 * x2_a + 0.4 * x2_b) / tot_specie_attese_trovate as f32;   
-  
+  let result = (0.6 * x2_a + 0.4 * x2_b) / tot_specie_attese_trovate as f32;
+
   Ok(result)
 }
 
@@ -62,7 +62,7 @@ fn calculate_sommatoria_x2_a(c: &CampionamentoNISECI) -> Result<f32, Vec<String>
 
   // riempo l'hashmap con solo le specie autoctone campionate
   for cattura in &c.campionamento {
-    if cattura.specie.tipo_autoctono == 1 || cattura.specie.tipo_autoctono == 2 {
+    if cattura.specie.specie_attesa && (cattura.specie.tipo_autoctono == 1 || cattura.specie.tipo_autoctono == 2) {
       match classi_eta_map.entry(cattura.specie.id.clone()) {
         Entry::Occupied(mut entry) => {
           entry.get_mut().update_classi_eta(&cattura);
@@ -71,7 +71,7 @@ fn calculate_sommatoria_x2_a(c: &CampionamentoNISECI) -> Result<f32, Vec<String>
           entry.insert(ClassiEtaSpecieNISECI::new_cl_prevalorizzata(&cattura));
         }
       };
-    } 
+    }
   }
 
   // ora la mappa è riempita e tutte le classi sono state riempite
@@ -97,11 +97,11 @@ fn calculate_sommatoria_x2_a(c: &CampionamentoNISECI) -> Result<f32, Vec<String>
 
 fn calculate_sommatoria_x2_b(c: &CampionamentoNISECI, anagrafica: &AnagraficaNISECI) -> Result<f32, Vec<String>> {
   let superficie = anagrafica.get_larghezza_media() * anagrafica.get_lunghezza_media();
-  
+
   let mut esemplari_per_cattura_map: HashMap<String, EsemplariPerCattura> = HashMap::with_capacity(10);
 
   for cattura in &c.campionamento {
-    if cattura.specie.tipo_autoctono == 1 || cattura.specie.tipo_autoctono == 2 {
+    if cattura.specie.specie_attesa && (cattura.specie.tipo_autoctono == 1 || cattura.specie.tipo_autoctono == 2) {
       match esemplari_per_cattura_map.entry(cattura.specie.id.clone()) {
           Entry::Occupied(mut occupied_entry) => {
             occupied_entry.get_mut().fill_passaggio(cattura.passaggio_cattura);
@@ -131,7 +131,7 @@ fn calculate_sommatoria_x2_b(c: &CampionamentoNISECI, anagrafica: &AnagraficaNIS
   }
 
   Ok(sommatoria_x2_b) // finally
-} 
+}
 
 fn update_classi_eta(cl: &mut ClassiEtaSpecieNISECI, record: &RecordNISECI) -> () {
   if record.lunghezza < record.specie.cl_soglia1 {
@@ -189,7 +189,7 @@ fn get_quantita_stimata(passaggi: &HashMap<u8, u32>) -> Result<u32, String> {
 }
 
 fn calculate_passaggi_ripetuti(c1: u32, c2: u32) -> Result<u32, String> {
-  
+
   match c1 == c2 || c1 == 0 || c2 == 0 {
     true => return Ok(c1 + c2),
     false => {},
@@ -197,7 +197,7 @@ fn calculate_passaggi_ripetuti(c1: u32, c2: u32) -> Result<u32, String> {
 
   let c = c1 + c2;
   let divisore = c2 as f32 / c1 as f32;
-  
+
   let result = (c as f32 / (1.0 - divisore.powf(2.0))).round() as i32;
 
 
@@ -215,12 +215,12 @@ fn calculate_q_stimata_regression(passaggi: &HashMap<u8, u32>) -> Result<u32, St
   // dalla mappa non riesco a capire se ci siano o meno dei passaggi in cui non è stato trovato pesce
   // quindi mi creo un vettore che rappresenta i pesci trovati per ogni passaggio in ordine di passaggio
   let mut esemplari_per_passaggio = vec![0 as u32; ultimo_passaggio as usize];
-  
+
   for (key, value) in passaggi {
     esemplari_per_passaggio[(*key - 1) as usize] = *value;
   }
 
-  // ora creo i punti con x == esemplari catturati fino a quel passaggio 
+  // ora creo i punti con x == esemplari catturati fino a quel passaggio
   // e y == esmplari catturati in quel passaggio
   let mut current_tot = 0;
   let points: Vec<Point<i32>> = esemplari_per_passaggio
@@ -266,7 +266,7 @@ mod x2_private_tests {
     assert!(q_stimata.is_ok());
     assert_eq!(225, q_stimata.unwrap());
   }
-  
+
   #[test]
   fn calcola_q_stimata_regression_same_values() {
     let mut passaggi: HashMap<u8, u32> = HashMap::new();
@@ -284,12 +284,12 @@ mod x2_private_tests {
   fn calcola_passaggi_ripetuti() {
 
     let q_stimata_1 = calculate_passaggi_ripetuti(30, 12);
-    
+
     assert!(q_stimata_1.is_ok());
     assert_eq!(q_stimata_1.unwrap(), 50);
 
     let q_stimata_2 = calculate_passaggi_ripetuti(30, 15);
-    
+
     assert!(q_stimata_2.is_ok());
     assert_eq!(q_stimata_2.unwrap(), 60);
   }
@@ -336,13 +336,13 @@ mod x2_private_tests {
 
     assert!(q_stimata.is_ok());
     assert_eq!(50, q_stimata.unwrap());
-    
+
     let mut passaggi2: HashMap<u8, u32> = HashMap::new();
     passaggi2.insert(1, 30);
     passaggi2.insert(2, 15);
-    
+
     let q_stimata2 = get_quantita_stimata(&passaggi2);
-    
+
     assert!(q_stimata2.is_ok());
     assert_eq!(60, q_stimata2.unwrap());
   }
@@ -727,4 +727,4 @@ mod x2_private_tests {
 
 
 
-} 
+}
